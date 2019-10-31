36da3d30e47901c47382a3ec2779933eea482c69
Alvaro Castro-Castilla <a@fourthbit.com>
2019-10-31 12:43:03 +0200

    Update to Go 1.13.3

--- gopathfs/attr_darwin.go
+++ gopathfs/attr_darwin.go
@@ -11,15 +11,15 @@ func unixAttrToFuseAttr(from unix.Stat_t) (result fuse.Attr) {
 	result.Blocks = uint64(from.Blocks)
 	result.Mode = uint32(from.Mode)
 
-	sec, nsec := from.Atimespec.Unix()
+	sec, nsec := from.Atim.Unix()
 	result.Atime = uint64(sec)
 	result.Atimensec = uint32(nsec)
 
-	sec, nsec = from.Ctimespec.Unix()
+	sec, nsec = from.Ctim.Unix()
 	result.Ctime = uint64(sec)
 	result.Ctimensec = uint32(nsec)
 
-	sec, nsec = from.Mtimespec.Unix()
+	sec, nsec = from.Mtim.Unix()
 	result.Mtime = uint64(sec)
 	result.Mtimensec = uint32(nsec)
 
